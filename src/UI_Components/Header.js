e6a4fc6963e96624e61a615991c557cee07df165
nate <niibobby4@gmail.com>
2022-05-22 23:33:10 +0000

    added styling for header

--- src/UI_Components/Header.js
+++ src/UI_Components/Header.js
@@ -1,9 +1,15 @@
 import classes from '../UI_Components/Header.module.css'
+import header_image from '../assets/house.png'
 
 const Header  = ()=>{
     return(
-        <div className={classes.header}>
+        <div className={classes.header }>
+            <div className={classes.partition_one}>
             <h1>How much can you afford to buy</h1>
+            </div>
+            <div className={classes.partition_two}>
+            <img src={header_image} alt="this is car image" />
+            </div>
         </div>
     )
 }
